e0a915e6592b87a46385756bee53bb1e54f2e6a8
keegan-trevill <157654717+keegan-trevill@users.noreply.github.com>
2024-02-21 16:44:01 -0600

    Activity 5
    Activity 5

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-// Add all scripts to the JS folder
+
+var map = L.map('map').setView([48.3794, 31.1656], 6);
+
+//Add tile layer to Map using OpenStreetMap
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
